1a68a4a96256e413e427fe61b90985ee8e429d08
Diego Fonseca <diego.fonseca@omnilatam.com>
2022-09-20 15:30:11 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='klym_erp_connector',
+    version='0.1.0',
+    description='A package to manage ERP integrations',
+    url='https://github.com/Dandresfsoto/klym_erp_connector',
+    author='Diego Fonseca',
+    author_email='diego.fonseca@omnilatam.com',
+    license='BSD 2-clause',
+    packages=['klym_erp_connector'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
